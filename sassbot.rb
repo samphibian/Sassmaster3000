453edd120fbd2d1568e8bd09fb3feddadd7a9a9a
user <sjkacir@gmail.com>
2018-01-01 18:38:48 -0500

    switched to bot token

--- sassbot.rb
+++ sassbot.rb
@@ -4,7 +4,7 @@ require 'json'
 file = File.read('config.json')
 info_hash = JSON.parse(file)
 
-bot = Discordrb::Bot.new token: info_hash['token'], client_id: info_hash['client_id']
+bot = Discordrb::Bot.new token: info_hash['bot_token']
 
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
