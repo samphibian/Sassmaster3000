d6aed40ce8421d4d8fff6f825c0f0a5493e459e3
user <sjkacir@gmail.com>
2018-01-01 18:35:11 -0500

    removed client id

--- sassbot.rb
+++ sassbot.rb
@@ -4,12 +4,10 @@ require 'json'
 file = File.read('config.json')
 info_hash = JSON.parse(file)
 
-bot = Discordrb::Bot.new token: info_hash['token'], client_id: 397524031235948545
+bot = Discordrb::Bot.new token: info_hash['token'], client_id: info_hash['client_id']
 
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
 end
-
-puts "This bot's invite URL is #{bot.invite_url}."
 
 bot.run
