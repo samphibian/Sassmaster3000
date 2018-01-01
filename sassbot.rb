358681e588fe9943c21f01d5ba6724c27fca17a2
user <sjkacir@gmail.com>
2018-01-01 18:22:06 -0500

    added gitignore

--- sassbot.rb
+++ sassbot.rb
@@ -1,9 +1,15 @@
 require 'discordrb'
+require 'json'
 
-bot = Discordrb::Bot.new token: '<token here>', client_id: 397524031235948545
+file = File.read('config.json')
+info_hash = JSON.parse(file)
+
+bot = Discordrb::Bot.new token: info_hash['token'], client_id: 397524031235948545
 
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
 end
+
+puts "This bot's invite URL is #{bot.invite_url}."
 
 bot.run
