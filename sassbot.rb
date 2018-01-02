bd2785f2c17017f85da8ea23c67c0361b792c65f
user <sjkacir@gmail.com>
2018-01-01 19:31:35 -0500

    added some test interactions

--- sassbot.rb
+++ sassbot.rb
@@ -3,11 +3,25 @@ require 'json'
 
 file = File.read('config.json')
 info_hash = JSON.parse(file)
+userArray = []
 
-bot = Discordrb::Bot.new token: info_hash['bot_token']
+bot = Discordrb::Commands::CommandBot.new token: info_hash['bot_token'], prefix: '/'
 
-bot.message(with_text: 'Ping!') do |event|
+bot.message(with_text: 'ping') do |event|
+  puts event
   event.respond 'Pong!'
+end
+
+bot.command(:sass) do |event, user|  
+  puts event
+  # user = event.message.content.split(' ')[1]
+  puts user
+  userArray.push(user)
+  return nil
+end
+
+bot.message(from: userArray) do |event|
+  event.respond 'plagiarize'
 end
 
 bot.run
